4908bdb490958240d55ca2e4762745a56bc17b98
joshua richards <jermain119@gmail.com>
2019-09-23 17:27:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,10 +3,12 @@
 def introduction(name)
   puts "hi, my name is #{name}."
 end
-# fi
+
+# method name  first parameter , second parameter
 def #introduction_with_language(name,language)
 puts "Hi, my name is #{name} and i am learning to program in #{language}."
 
+#method name, second parameter defualt value language ="ruby"
 def #introduction_with_language_optional(name,language= "Ruby")
 puts "Hi, my name is #{name} and i am learning to program in #{language}."
 
